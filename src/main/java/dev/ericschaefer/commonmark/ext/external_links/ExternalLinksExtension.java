788c93bb68acd32384ac990dcc67fca25413bcb5
Eric Schaefer <eric.schaefer@ericschaefer.dev>
2021-01-07 16:50:41 +0100

    Build, Sign, Deploy

--- src/main/java/dev/ericschaefer/commonmark/ext/external_links/ExternalLinksExtension.java
+++ src/main/java/dev/ericschaefer/commonmark/ext/external_links/ExternalLinksExtension.java
@@ -3,6 +3,11 @@ package dev.ericschaefer.commonmark.ext.external_links;
 import org.commonmark.Extension;
 import org.commonmark.renderer.html.HtmlRenderer;
 
+/**
+ * commonmark-java extension for adding the attributes target="_blank" and rel="noopener" to links
+ *
+ * Create via ExternalLinksExtension.create(baseUrl) and add to extension in parser and render build steps.
+ */
 public class ExternalLinksExtension implements HtmlRenderer.HtmlRendererExtension {
     private final String baseUrl;
 
@@ -10,6 +15,15 @@ public class ExternalLinksExtension implements HtmlRenderer.HtmlRendererExtensio
         this.baseUrl = baseUrl;
     }
 
+    /**
+     * Create the extension.
+     *
+     * Example: ExternalLinksExtension.create("https://mydomain.com")
+     *
+     * @param baseUrl The base URL to use for detecting external links
+     * @return the extension
+     *
+     */
     public static Extension create(String baseUrl) {
         return new ExternalLinksExtension(baseUrl);
     }
